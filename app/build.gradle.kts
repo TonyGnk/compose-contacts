0926f6bab9f2448c894c95bb860e6936e1ced34f
TonyGnk <61454221+TonyGnk@users.noreply.github.com>
2024-04-12 21:40:02 +0300

    Uncompleted Splash Screen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,6 +61,9 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    //implementation(libs.androidx.core.splash.screen)
+    implementation("androidx.core:core-splashscreen:1.0.1")
+
 
     implementation(libs.androidx.navigation.compose)
 
--- app/src/main/java/com/example/composecontacts/MainActivity.kt
+++ app/src/main/java/com/example/composecontacts/MainActivity.kt
@@ -10,12 +10,19 @@ import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.composecontacts.ui.theme.ComposeContactsTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        enableEdgeToEdge()
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+
+        installSplashScreen().apply {
+            
+        }
+
         setContent {
             ComposeContactsTheme {
                 // A surface container using the 'background' color from the theme
